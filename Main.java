29c922734c99660cfee387a5a54507c9e0563c50
Trieu Vu <134004611+trieuPhiVu@users.noreply.github.com>
2023-10-11 13:28:55 +0200

    Aufgabe von Dienstag(11/10)

--- Main.java
+++ Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Azubi vu = new Azubi("Trieu", "Vu", 1000, 2);
-        vu.setPruefungen(4);
-        vu.addZulage(500);
-        vu.zeigeDaten();
-
-        Angestellter ngoc = new Angestellter("Vu", "Ngoc", 3000, 1);
-        ngoc.befoerdere();
-        ngoc.addZulage(2000);
-        ngoc.zeigeDaten();
-
+        System.out.println(StringAufgabe.reverseString("RETEP"));
+        System.out.println(StringAufgabe.isPalindrome("ABCXcba"));
     }
 }
